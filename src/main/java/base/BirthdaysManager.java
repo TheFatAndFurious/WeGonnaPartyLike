694c2342de9a24f860c227c34aff9afd2997eb5c
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-13 23:58:20 +0200

    Birthdays class

---
+++ src/main/java/base/BirthdaysManager.java
@@ -0,0 +1,33 @@
+package base;
+
+import java.util.Date;
+
+public class BirthdaysManager {
+    String givenName;
+    String familyName;
+    Date birthdate;
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+}
---
+++ src/main/java/base/Main.java
@@ -0,0 +1,8 @@
+package base;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Alive and well");
+        // TODO: Create a "Birthday class" that will take a date and name as fields
+    }
+}
