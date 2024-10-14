bc38b750296fbdc22ab2e80f20afb5106e6e7f1c
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-14 22:53:56 +0200

    Now taking user input

--- src/main/java/base/TaSoeur.java
+++ src/main/java/base/TaSoeur.java
@@ -1,15 +1,29 @@
 package base;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class TaSoeur {
     public static void main(String[] args){
         System.out.println("Alive and well");
-        // TODO: Create a "Birthday class" that will take a date and name as fields
+
+        Scanner scanner = new Scanner(System.in);
         BirthdaysManager birthdayJulie = new BirthdaysManager();
-        birthdayJulie.setFamilyName("Goineau");
-        birthdayJulie.setGivenName("Julie");
-        LocalDate date = LocalDate.of(1990, 11, 6);
+
+        System.out.println("Please enter a given name");
+        String givenName = scanner.nextLine();
+        birthdayJulie.setGivenName(givenName);
+        System.out.println("Please enter a family name");
+        String familyName = scanner.nextLine();
+        birthdayJulie.setFamilyName(familyName);
+        System.out.println("Please enter a day");
+        int dayOfBirth = scanner.nextInt();
+        System.out.println("Please enter a month");
+        int monthOfBirth = scanner.nextInt();
+        System.out.println("Please enter a year");
+        int yearOfBirth = scanner.nextInt();
+        LocalDate date = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
         birthdayJulie.setBirthdate(date);
 
         System.out.println("L'anniversaire de " + birthdayJulie.getGivenName() + " " + birthdayJulie.getFamilyName() + " est le: " + birthdayJulie.getBirthdate());
