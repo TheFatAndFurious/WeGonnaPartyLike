5a5b829abca0b5742c69f0746192466f8688850c
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-15 21:18:27 +0200

    Create table

--- src/main/java/base/Database.java
+++ src/main/java/base/Database.java
@@ -3,6 +3,7 @@ package base;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
     private static final String JDBC_url = "jdbc:h2:./data/database";
@@ -11,5 +12,21 @@ public class Database {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_url, USER, PASSWORD);
+    }
+
+    public static void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS birthdays (" +
+                    "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                    "givenName VARCHAR (255) NOT NULL, " +
+                    "familyName VARCHAR (255) NOT NULL, " +
+                    "birthdate DATE NOT NULL, " +
+                    ");";
+
+        try {
+            Connection conn = getConnection();
+            Statement smt = conn.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
