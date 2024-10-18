a12d5ff1ec1aaa43a50e96830ffad892a519fdb7
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-18 10:54:22 +0200

    Message helper done

--- src/main/java/base/MessageHelper.java
+++ src/main/java/base/MessageHelper.java
@@ -2,7 +2,18 @@ package base;
 
 public class MessageHelper {
 
-    public static void MessagePrint(String message) {
+    public static void PrintMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void PrintFormattedMessage(String message, Object... args){
+        System.out.println(String.format(message, args));
+    }
+
+    public static void PrintErrorMessage(String message, Exception e){
+        System.out.println("ERROR: " + message);
+        if (e != null){
+            e.printStackTrace(System.err);
+        }
     }
 }
