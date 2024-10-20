545e1e2539f7523c63c4be1301adf34f80012e69
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-20 15:23:30 +0200

    DataSource class done

--- src/main/java/base/SimpleDataSource.java
+++ src/main/java/base/SimpleDataSource.java
@@ -3,53 +3,65 @@ package base;
 import javax.sql.DataSource;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.logging.Logger;
 
 public class SimpleDataSource implements DataSource {
+
+    private String jdbcUrl;
+    private String username;
+    private String password;
+
+    public SimpleDataSource(String jdbcUrl, String username, String password){
+        this.jdbcUrl = jdbcUrl;
+        this.username = username;
+        this.password = password;
+    }
+
     @Override
     public Connection getConnection() throws SQLException {
-        return null;
+        return DriverManager.getConnection(jdbcUrl, username, password);
     }
 
     @Override
     public Connection getConnection(String s, String s1) throws SQLException {
-        return null;
+        return DriverManager.getConnection(jdbcUrl, username, password);
     }
 
     @Override
     public PrintWriter getLogWriter() throws SQLException {
-        return null;
+        throw new UnsupportedOperationException("Not implemented.");
     }
 
     @Override
     public void setLogWriter(PrintWriter printWriter) throws SQLException {
-
+        throw new UnsupportedOperationException("Not implemented.");
     }
 
     @Override
     public void setLoginTimeout(int i) throws SQLException {
-
+        throw new UnsupportedOperationException("Not implemented.");
     }
 
     @Override
     public int getLoginTimeout() throws SQLException {
-        return 0;
+        throw new UnsupportedOperationException("Not implemented.");
     }
 
     @Override
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return null;
+        throw new UnsupportedOperationException("Not implemented.");
     }
 
     @Override
     public <T> T unwrap(Class<T> aClass) throws SQLException {
-        return null;
+        throw new UnsupportedOperationException("Not implemented.");
     }
 
     @Override
     public boolean isWrapperFor(Class<?> aClass) throws SQLException {
-        return false;
+        throw new UnsupportedOperationException("Not implemented.");
     }
 }
