a6c8a1328fa86203febed5f0c7bee8189ffed8be
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-24 14:05:20 +0200

    DeleteBirthday class

---
+++ src/main/java/base/DeleteBirthdayCommand.java
@@ -0,0 +1,22 @@
+package base;
+
+import javax.xml.crypto.Data;
+
+public class DeleteBirthdayCommand implements Command{
+    MessageHelper messageHelper;
+    InputHelper inputHelper;
+    Database database;
+
+    DeleteBirthdayCommand(MessageHelper messageHelper, InputHelper inputHelper, Database database){
+        this.messageHelper = messageHelper;
+        this.inputHelper = inputHelper;
+        this.database = database;
+    }
+
+
+    @Override
+    public void execute() {
+
+
+    }
+}
