381f7867c2b818ee62c0fe4733c44c04e3c363d2
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-28 15:50:39 +0100

    Application class

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,4 +1,15 @@
 package base;
 
 public class Application {
+    private Database database;
+    private MessageHelper messageHelper;
+    private InputHelper inputHelper;
+    private MenuManager menuManager;
+
+
+    private void initializeCommands(){
+        Command addBirthday = new AddBirthdayCommand(database, messageHelper, inputHelper);
+        Command deleteBirthday = new DeleteBirthdayCommand(messageHelper, inputHelper, database);
+        Command listBirthdays = new ListBirthdaysCommand(database, messageHelper);
+    }
 }
