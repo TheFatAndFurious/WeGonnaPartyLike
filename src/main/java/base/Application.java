b2d9e459004671872151befc10590b360fbbcb4f
TheFatAndFurious <mrguerrilla@gmail.com>
2024-11-09 14:39:12 +0100

    refactoring main & app class

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,28 +1,62 @@
 package base;
 
+import base.util.ConsoleInputHelper;
 import base.util.InputHelper;
 import base.util.MessageHelper;
+import config.Config;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+
+/**
+ * This class will be used to instantiate everything we need to start and run the app
+ */
+
 public class Application {
     private Database database;
-    private MessageHelper messageHelper;
     private InputHelper inputHelper;
     private Map<Integer, MenuOption> menuOptionsMap;
     private MenuManager menuManager;
-
-    public Application(Database database, MessageHelper messageHelper, InputHelper inputHelper){
+    private MessageHelper messageHelper;
+    private Config config;
+    public Application(Database database){
         this.database = database;
-        this.messageHelper = messageHelper;
-        this.inputHelper = inputHelper;
+    }
 
+    /**
+     * Initiating a Config object to be able the reach the config file
+     */
+    //TODO: Should we pass a param to allow to use different config files ?
+    public void initConfig(){
+        this.config = new Config();
+    }
+
+    public void initHelpers() {
+        this.messageHelper = new MessageHelper();
+        this.inputHelper = new ConsoleInputHelper();
+    }
+
+    public void initializeDatabase() throws SQLException {
+        String jdbcUrl = config.getDBUrl();
+        String username = config.getDBUsername();
+        String password = config.getDBPassword();
+        DataSource dataSource = new SimpleDataSource(jdbcUrl, username, password);
+        Database database = new Database(dataSource, messageHelper);
+        try{
+            database.createTable();
+        } catch (RuntimeException e) {
+            messageHelper.PrintFormattedMessage(Messages.ERROR_MESSAGE, e.getMessage());
+            System.exit(1);
+        }
+    }
+    public void initializeApp(){
         menuOptionsMap = new LinkedHashMap<>();
         menuManager = new MenuManager(inputHelper, messageHelper, menuOptionsMap);
         initializeCommands();
     }
-
 
 
     private void initializeCommands(){
--- src/main/java/base/TaSoeur.java
+++ src/main/java/base/TaSoeur.java
@@ -13,21 +13,6 @@ import java.time.LocalDate;
 public class TaSoeur {
     public static void main(String[] args) throws SQLException, EmailException {
         System.out.println("Welcome my guy");
-        Config config =new Config();
-        String jdbcUrl = config.getDBUrl();
-        String username = config.getDBUsername();
-        String password = config.getDBPassword();
-        InputHelper inputHelper = new ConsoleInputHelper();
-        MessageHelper messageHelper = new MessageHelper();
-
-        DataSource dataSource = new SimpleDataSource(jdbcUrl, username, password);
-        Database database = new Database(dataSource, messageHelper);
-        try{
-            database.createTable();
-        } catch (RuntimeException e) {
-            messageHelper.PrintFormattedMessage(Messages.ERROR_MESSAGE, e.getMessage());
-            System.exit(1);
-        }
         ScheduleTaskManager scheduleTaskManager = new ScheduleTaskManager();
         EmailService emailService = new EmailService();
         TasksManager tasksManager = new TasksManager(database, emailService);
@@ -35,7 +20,7 @@ public class TaSoeur {
         if (task != null) {
             scheduleTaskManager.runService(task);
         }
-        Application application = new Application(database, messageHelper, inputHelper);
+        Application application = new Application(database);
         application.start();
 
     }
